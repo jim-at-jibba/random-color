af13fc2866b301fe9ffea622f75a3ccdf5efe610
James Best <james.best@breedr.co>
2023-06-23 07:44:08 +0100

    Set up table view

--- random-color/ColorsTableVCViewController.swift
+++ random-color/ColorsTableVCViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ColorsTableVCViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,17 +15,24 @@ class ColorsTableVCViewController: UIViewController {
     }
     
 
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
+    
+
+
+}
+
+// extending is the same as having them above its just a bit cleaner
+// Now all the tableView code is separate and cleaner
+extension ColorsTableVCViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+   
+    // delegate is a function that waits for an action. When that action happens this delegate function runs
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+   
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ToColorsDetailsVC", sender: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
